95a1729731b51a0bd4cc230816c27fe596dbcece
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-04 23:05:41 +0100

    Add rust crates dynamips_c, dynamips_c_unstable.
    The focus is a simple C-to-rust conversion of dynamips.
    Safe rust code will be developed and placed in other crates as needed.

---
+++ rust/dynamips_c/src/lib.rs
@@ -0,0 +1,4 @@
+//! This crate contains a C-to-rust conversion of dynamips.
+//!
+//! The focus of this crate is a simple C-to-rust conversion of dynamips.
+//! Safe rust code will be developed and placed in other crates as needed.

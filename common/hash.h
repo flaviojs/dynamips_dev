de7b72eaa63c5692bd44eb7a1dcb438aa9b6498b
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-05 23:21:31 +0100

    Rustify hash.

--- common/hash.h
+++ common/hash.h
@@ -13,47 +13,6 @@
 #include <sys/types.h>
 #include "utils.h"
 
-/* Key computation function */
-typedef u_int (*hash_fcompute)(void *key);
-
-/* Comparison function for 2 keys */
-typedef int (*hash_fcompare)(void *key1,void *key2);
-
-/* User function to call when using hash_table_foreach */
-typedef void (*hash_fforeach)(void *key,void *value,void *opt_arg);
-
-/* Hash element (pair key,value) */
-typedef struct hash_node hash_node_t;
-struct hash_node {
-   void *key, *value;
-   hash_node_t *next;
-};
-
-/* Hash Table definition */
-typedef struct hash_table hash_table_t;
-struct hash_table {
-   int size,nnodes;
-   hash_node_t **nodes;
-   hash_fcompute hash_func;
-   hash_fcompare key_cmp;
-};
-
-#define hash_string_create(hash_size) \
-   hash_table_create(str_hash,str_equal,hash_size)
-
-#define hash_int_create(hash_size) \
-   hash_table_create(int_hash,int_equal,hash_size)
-
-#define hash_u64_create(hash_size) \
-   hash_table_create(u64_hash,u64_equal,hash_size)
-
-#define hash_ptr_create(hash_size) \
-   hash_table_create(ptr_hash,ptr_equal,hash_size)
-
-#define HASH_TABLE_FOREACH(i,ht,hn) \
-   for(i=0;i<ht->size;i++) \
-      for(hn=ht->nodes[i];hn;hn=hn->next)
-
 /* Create a new hash table */
 hash_table_t *hash_table_create(hash_fcompute hash_func,hash_fcompare key_cmp,
                                 int hash_size);

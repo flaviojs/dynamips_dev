9020ec620aa30b2cd72cb3d61400280f89faf5aa
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-08 22:42:23 +0100

    Rustify sbox.
    Fix infinite loop in sbox_compute.

--- common/sbox.h
+++ common/sbox.h
@@ -12,39 +12,4 @@
 
 #include "dynamips_common.h"
 
-extern m_uint32_t sbox_array[];
-
-static inline m_uint32_t sbox_compute(m_uint8_t *data,int len)
-{
-   m_uint32_t hash = 0;
-   
-   while(len > 0) {
-      hash ^= sbox_array[*data];
-      hash *= 3;
-      data++;
-   }
-
-   return(hash);
-}
-
-static forced_inline m_uint32_t sbox_u32(m_uint32_t val)
-{
-   m_uint32_t hash = 0;
-
-   hash ^= sbox_array[(m_uint8_t)val];
-   hash *= 3;
-   val >>= 8;
-
-   hash ^= sbox_array[(m_uint8_t)val];
-   hash *= 3;
-   val >>= 8;
-
-   hash ^= sbox_array[(m_uint8_t)val];
-   hash *= 3;
-   val >>= 8;
-
-   hash ^= sbox_array[(m_uint8_t)val];
-   return(hash);
-}
-
 #endif

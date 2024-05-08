5dfe9b529fa7688ee4a5e5c3e4e33a1904fb21ad
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-08 17:14:20 +0100

    Rustify crc.

--- rust/dynamips-c/src/crc.rs
+++ rust/dynamips-c/src/crc.rs
@@ -1,1 +1,119 @@
+//! Cisco router simulation platform.
+//! Copyright (c) 2006 Christophe Fillot (cf@utc.fr)
+//!
+//! CRC functions.
 
+use crate::_private::*;
+use crate::dynamips_common::*;
+
+/// Compute a CRC-12 hash on a 32-bit integer
+#[inline(always)]
+#[no_mangle]
+pub unsafe extern "C" fn crc12_hash_u32(mut val: m_uint32_t) -> m_uint32_t {
+    let mut crc: m_uint32_t = 0;
+
+    for _ in 0..4 {
+        crc = (crc >> 8) ^ crc12_array[((crc ^ val) & 0xff) as usize] as m_uint32_t;
+        val >>= 8;
+    }
+
+    crc
+}
+
+/// Compute a CRC-16 hash on a 32-bit integer
+#[inline(always)]
+#[no_mangle]
+pub unsafe extern "C" fn crc16_hash_u32(mut val: m_uint32_t) -> m_uint32_t {
+    let mut crc: m_uint32_t = 0;
+
+    for _ in 0..4 {
+        crc = (crc >> 8) ^ crc16_array[((crc ^ val) & 0xff) as usize] as m_uint32_t;
+        val >>= 8;
+    }
+
+    crc
+}
+
+/// Compute a CRC-32 on the specified block
+#[inline(always)]
+#[no_mangle]
+pub unsafe extern "C" fn crc32_compute(crc_accum: m_uint32_t, ptr: *mut u8, len: c_int) -> m_uint32_t {
+    let mut c: m_uint32_t = crc_accum;
+
+    for n in 0..len {
+        c = crc32_array[((c ^ *ptr.offset(n as isize) as m_uint32_t) & 0xff) as usize] ^ (c >> 8);
+    }
+
+    !c
+}
+
+const CRC12_POLY: m_uint16_t = 0x0f01;
+const CRC16_POLY: m_uint16_t = 0xa001;
+const CRC32_POLY: c_ulong = 0xedb88320;
+
+/// CRC tables
+static mut crc12_array: [m_uint16_t; 256] = [0; 256];
+static mut crc16_array: [m_uint16_t; 256] = [0; 256];
+static mut crc32_array: [m_uint32_t; 256] = [0; 256];
+
+/// Initialize CRC-12 algorithm
+unsafe fn crc12_init() {
+    for i in 0..256 as c_int {
+        let mut crc: m_uint16_t = 0;
+        let mut c: m_uint16_t = i as m_uint16_t;
+        for _ in 0..8 {
+            if ((crc ^ c) & 0x0001) != 0 {
+                crc = (crc >> 1) ^ CRC12_POLY;
+            } else {
+                crc >>= 1;
+            }
+
+            c >>= 1;
+        }
+
+        crc12_array[i as usize] = crc;
+    }
+}
+
+/// Initialize CRC-16 algorithm
+unsafe fn crc16_init() {
+    for i in 0..256 as c_int {
+        let mut crc: m_uint16_t = 0;
+        let mut c: m_uint16_t = i as m_uint16_t;
+
+        for _ in 0..8 {
+            if ((crc ^ c) & 0x0001) != 0 {
+                crc = (crc >> 1) ^ CRC16_POLY;
+            } else {
+                crc >>= 1;
+            }
+
+            c >>= 1;
+        }
+
+        crc16_array[i as usize] = crc;
+    }
+}
+
+/// Initialize CRC-32 algorithm
+unsafe fn crc32_init() {
+    for n in 0..256 as c_int {
+        let mut c: c_ulong = n as c_ulong;
+        for _ in 0..8 {
+            if (c & 1) != 0 {
+                c = CRC32_POLY ^ (c >> 1);
+            } else {
+                c >>= 1;
+            }
+        }
+        crc32_array[n as usize] = c as m_uint32_t;
+    }
+}
+
+/// Initialize CRC algorithms
+#[no_mangle]
+pub unsafe extern "C" fn crc_init() {
+    crc12_init();
+    crc16_init();
+    crc32_init();
+}

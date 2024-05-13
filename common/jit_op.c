79047650f90350741beee9dedbecc3c27f5aac17
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-13 12:55:24 +0100

    Rustify jit_op_get.

--- common/jit_op.c
+++ common/jit_op.c
@@ -17,33 +17,3 @@
 
 #include "cpu.h"
 #include "jit_op.h"
-
-/* Get a JIT op (allocate one if necessary) */
-jit_op_t *jit_op_get(jit_op_data_t *data,int size_index,u_int opcode)
-{
-   jit_op_t *op;
-   size_t len;
-
-   assert(size_index < JIT_OP_POOL_NR);
-   op = data->pool[size_index];
-
-   if (op != NULL) {
-      assert(op->ob_size_index == size_index);
-      data->pool[size_index] = op->next;
-   } else {
-      /* no block found, allocate one */
-      len = sizeof(*op) + jit_op_blk_sizes[size_index];
-
-      op = malloc(len);
-      assert(op != NULL);
-      op->ob_size_index = size_index;
-   }
-
-   op->opcode = opcode;
-   op->param[0] = op->param[1] = op->param[2] = -1;
-   op->next = NULL;
-   op->ob_ptr = op->ob_data;
-   op->arg_ptr = NULL;
-   op->insn_name = NULL;
-   return op;
-}

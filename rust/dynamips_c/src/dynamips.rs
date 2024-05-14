730dd66730d27b8b4526e5bee668ba2d7e7ee2fe
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-14 02:35:10 +0100

    Rustify cbm_hash_f, cbm_array_t, CBM_*, cbm_cmp_f, cbm_set_rule, cbm_unset_rule, cbm_check_rule, cbm_bitwise_and, cbm_create, cbm_duplicate, cbm_get_eqclass, rfc_eqclass_t, rfc_alloc_array, rfc_free_array, rfc_free_array_cbm_hash_value, rfc_check_insn, rfc_phase_0, rfc_phase_j, ilt_phase_0, ilt_phase_j, ilt_postprocessing, ilt_compile, ilt_store_rfct, ilt_store_table, ilt_load_rfct, ilt_check_cached_table, ilt_destroy, ilt_load_table, ilt_build_filename, ilt_cache_load, ilt_cache_store, ilt_create, ilt_get_index, ilt_get_idx, ilt_lookup.

--- rust/dynamips_c/src/dynamips.rs
+++ rust/dynamips_c/src/dynamips.rs
@@ -5,3 +5,9 @@
 //! Many thanks to Nicolas Szalay for his patch
 //! for the command line parsing and virtual machine
 //! settings (RAM, ROM, NVRAM, ...)
+
+use crate::_private::*;
+
+/// Software version tag
+#[no_mangle]
+pub static mut sw_version_tag: *const c_char = cstr!("2023010200");

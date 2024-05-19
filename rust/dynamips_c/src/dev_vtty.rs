bda34e5ddaa3f67318ec3fcca2435abddd9a3de3
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-05-19 09:56:55 +0100

    Rustify vtty_get_char.

--- rust/dynamips_c/src/dev_vtty.rs
+++ rust/dynamips_c/src/dev_vtty.rs
@@ -797,6 +797,27 @@ pub unsafe extern "C" fn vtty_read(vtty: *mut vtty_t, fd_slot: *mut c_int) -> c_
     }
 }
 
+/// Read a character from the buffer (-1 if the buffer is empty)
+#[no_mangle]
+pub unsafe extern "C" fn vtty_get_char(vtty: *mut vtty_t) -> c_int {
+    VTTY_LOCK(vtty);
+
+    if (*vtty).read_ptr == (*vtty).write_ptr {
+        VTTY_UNLOCK(vtty);
+        return -1;
+    }
+
+    let c: c_uchar = (*vtty).buffer[(*vtty).read_ptr as usize];
+    (*vtty).read_ptr += 1;
+
+    if (*vtty).read_ptr == VTTY_BUFFER_SIZE as c_uint {
+        (*vtty).read_ptr = 0;
+    }
+
+    VTTY_UNLOCK(vtty);
+    c.into()
+}
+
 /// Flush VTTY output
 #[no_mangle]
 pub unsafe extern "C" fn vtty_flush(vtty: *mut vtty_t) {

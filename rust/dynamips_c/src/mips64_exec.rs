4810157a18b6e3d859a023272bb96bbba11f9752
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-01 20:05:15 +0100

    Rustify mips64_exec_DMTC0.

--- rust/dynamips_c/src/mips64_exec.rs
+++ rust/dynamips_c/src/mips64_exec.rs
@@ -9,6 +9,7 @@ extern "C" {
     fn mips64_cp0_exec_cfc0(cpu: *mut cpu_mips_t, gp_reg: u_int, cp0_reg: u_int);
     fn mips64_cp0_exec_ctc0(cpu: *mut cpu_mips_t, gp_reg: u_int, cp0_reg: u_int);
     fn mips64_cp0_exec_dmfc0(cpu: *mut cpu_mips_t, gp_reg: u_int, cp0_reg: u_int);
+    fn mips64_cp0_exec_dmtc0(cpu: *mut cpu_mips_t, gp_reg: u_int, cp0_reg: u_int);
     fn mips64_exec_bdslot(cpu: *mut cpu_mips_t);
     fn mips64_exec_break(cpu: *mut cpu_mips_t, code: u_int);
     fn mips64_exec_dmfc1(cpu: *mut cpu_mips_t, gp_reg: u_int, cp1_reg: u_int);
@@ -735,5 +736,16 @@ pub unsafe extern "C" fn mips64_exec_DMFC1(cpu: *mut cpu_mips_t, insn: mips_insn
     let rd: c_int = bits(insn, 11, 15);
 
     mips64_exec_dmfc1(cpu, rt as u_int, rd as u_int);
+    0
+}
+
+/// DMTC0
+#[no_mangle] // TODO private
+#[cfg_attr(feature = "fastcall", abi("fastcall"))]
+pub unsafe extern "C" fn mips64_exec_DMTC0(cpu: *mut cpu_mips_t, insn: mips_insn_t) -> c_int {
+    let rt: c_int = bits(insn, 16, 20);
+    let rd: c_int = bits(insn, 11, 15);
+
+    mips64_cp0_exec_dmtc0(cpu, rt as u_int, rd as u_int);
     0
 }

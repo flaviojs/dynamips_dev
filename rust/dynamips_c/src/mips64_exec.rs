96e7613600d05afa939428bace44a37a7cb22baf
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-01 03:58:37 +0100

    Rustify mips64_exec_CFC0.

--- rust/dynamips_c/src/mips64_exec.rs
+++ rust/dynamips_c/src/mips64_exec.rs
@@ -6,6 +6,7 @@ use crate::prelude::*;
 use crate::utils::*;
 
 extern "C" {
+    fn mips64_cp0_exec_cfc0(cpu: *mut cpu_mips_t, gp_reg: u_int, cp0_reg: u_int);
     fn mips64_exec_bdslot(cpu: *mut cpu_mips_t);
     fn mips64_exec_break(cpu: *mut cpu_mips_t, code: u_int);
 }
@@ -628,5 +629,16 @@ pub unsafe extern "C" fn mips64_exec_CACHE(cpu: *mut cpu_mips_t, insn: mips_insn
     let offset: c_int = bits(insn, 0, 15);
 
     mips64_exec_memop2(cpu, MIPS_MEMOP_CACHE as c_int, base as m_uint64_t, offset, op as u_int, FALSE);
+    0
+}
+
+/// CFC0
+#[no_mangle] // TODO private
+#[cfg_attr(feature = "fastcall", abi("fastcall"))]
+pub unsafe extern "C" fn mips64_exec_CFC0(cpu: *mut cpu_mips_t, insn: mips_insn_t) -> c_int {
+    let rt: c_int = bits(insn, 16, 20);
+    let rd: c_int = bits(insn, 11, 15);
+
+    mips64_cp0_exec_cfc0(cpu, rt as u_int, rd as u_int);
     0
 }
--- stable/mips64_exec.c
+++ stable/mips64_exec.c
@@ -476,16 +476,6 @@ forced_inline void mips64_exec_bdslot(cpu_mips_t *cpu)
    mips64_exec_single_instruction(cpu,insn);
 }
 
-/* CFC0 */
-static fastcall int mips64_exec_CFC0(cpu_mips_t *cpu,mips_insn_t insn)
-{	
-   int rt = bits(insn,16,20);
-   int rd = bits(insn,11,15);
-
-   mips64_cp0_exec_cfc0(cpu,rt,rd);
-   return(0);
-}
-
 /* CTC0 */
 static fastcall int mips64_exec_CTC0(cpu_mips_t *cpu,mips_insn_t insn)
 {	

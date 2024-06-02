7668c7a76048f4e7acecdaff350b2cf39b01560b
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-02 09:33:32 +0100

    Rustify mips64_exec_J.

--- rust/dynamips_c/src/mips64_exec.rs
+++ rust/dynamips_c/src/mips64_exec.rs
@@ -890,3 +890,21 @@ pub unsafe extern "C" fn mips64_exec_ERET(cpu: *mut cpu_mips_t, _insn: mips_insn
     mips64_exec_eret(cpu);
     1
 }
+
+/// J
+#[no_mangle] // TODO private
+#[cfg_attr(feature = "fastcall", abi("fastcall"))]
+pub unsafe extern "C" fn mips64_exec_J(cpu: *mut cpu_mips_t, insn: mips_insn_t) -> c_int {
+    let instr_index: u_int = bits(insn, 0, 25) as u_int;
+
+    /* compute the new pc */
+    let mut new_pc: m_uint64_t = (*cpu).pc & !((1 << 28) - 1);
+    new_pc |= (instr_index << 2) as m_uint64_t;
+
+    // exec the instruction in the delay slot
+    mips64_exec_bdslot(cpu);
+
+    // set the new pc
+    (*cpu).pc = new_pc;
+    1
+}

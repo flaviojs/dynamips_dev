79e4cc239b30bcd42167e7e9432c3bc3ca1484ff
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-02 11:16:04 +0100

    Rustify mips64_exec_LDR.

--- rust/dynamips_c/src/mips64_exec.rs
+++ rust/dynamips_c/src/mips64_exec.rs
@@ -1027,3 +1027,15 @@ pub unsafe extern "C" fn mips64_exec_LDL(cpu: *mut cpu_mips_t, insn: mips_insn_t
     mips64_exec_memop2(cpu, MIPS_MEMOP_LDL as c_int, base as m_uint64_t, offset, rt as u_int, TRUE);
     0
 }
+
+/// LDR (Load Double-Word Right)
+#[no_mangle] // TODO private
+#[cfg_attr(feature = "fastcall", abi("fastcall"))]
+pub unsafe extern "C" fn mips64_exec_LDR(cpu: *mut cpu_mips_t, insn: mips_insn_t) -> c_int {
+    let base: c_int = bits(insn, 21, 25);
+    let rt: c_int = bits(insn, 16, 20);
+    let offset: c_int = bits(insn, 0, 15);
+
+    mips64_exec_memop2(cpu, MIPS_MEMOP_LDR as c_int, base as m_uint64_t, offset, rt as u_int, TRUE);
+    0
+}

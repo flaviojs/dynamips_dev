371732f75c87c69e54c103989a2d22b7cca38f9b
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-02 10:57:21 +0100

    Rustify mips64_exec_LB.

--- rust/dynamips_c/src/mips64_exec.rs
+++ rust/dynamips_c/src/mips64_exec.rs
@@ -967,3 +967,15 @@ pub unsafe extern "C" fn mips64_exec_JR(cpu: *mut cpu_mips_t, insn: mips_insn_t)
     (*cpu).pc = new_pc;
     1
 }
+
+/// LB (Load Byte)
+#[no_mangle] // TODO private
+#[cfg_attr(feature = "fastcall", abi("fastcall"))]
+pub unsafe extern "C" fn mips64_exec_LB(cpu: *mut cpu_mips_t, insn: mips_insn_t) -> c_int {
+    let base: c_int = bits(insn, 21, 25);
+    let rt: c_int = bits(insn, 16, 20);
+    let offset: c_int = bits(insn, 0, 15);
+
+    mips64_exec_memop2(cpu, MIPS_MEMOP_LB as c_int, base as m_uint64_t, offset, rt as u_int, TRUE);
+    0
+}
--- stable/mips64_exec.c
+++ stable/mips64_exec.c
@@ -476,17 +476,6 @@ forced_inline void mips64_exec_bdslot(cpu_mips_t *cpu)
    mips64_exec_single_instruction(cpu,insn);
 }
 
-/* LB (Load Byte) */
-static fastcall int mips64_exec_LB(cpu_mips_t *cpu,mips_insn_t insn)
-{
-   int base   = bits(insn,21,25);
-   int rt     = bits(insn,16,20);
-   int offset = bits(insn,0,15);
-
-   mips64_exec_memop2(cpu,MIPS_MEMOP_LB,base,offset,rt,TRUE);
-   return(0);
-}
-
 /* LBU (Load Byte Unsigned) */
 static fastcall int mips64_exec_LBU(cpu_mips_t *cpu,mips_insn_t insn)
 {

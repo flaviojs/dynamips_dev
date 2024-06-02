c7e56c0068a29a36ef9b6627e4539a317e35b953
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-02 12:27:18 +0100

    Rustify mips64_exec_LWU.

--- stable/mips64_exec.c
+++ stable/mips64_exec.c
@@ -476,17 +476,6 @@ forced_inline void mips64_exec_bdslot(cpu_mips_t *cpu)
    mips64_exec_single_instruction(cpu,insn);
 }
 
-/* LWU (Load Word Unsigned) */
-static fastcall int mips64_exec_LWU(cpu_mips_t *cpu,mips_insn_t insn)
-{
-   int base   = bits(insn,21,25);
-   int rt     = bits(insn,16,20);
-   int offset = bits(insn,0,15);
-
-   mips64_exec_memop2(cpu,MIPS_MEMOP_LWU,base,offset,rt,TRUE);
-   return(0);
-}
-
 /* MFC0 */
 static fastcall int mips64_exec_MFC0(cpu_mips_t *cpu,mips_insn_t insn)
 {	

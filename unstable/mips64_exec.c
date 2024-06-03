9ef9558667251548d24ece4167116bbfcba32334
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-03 15:43:58 +0100

    Rustify mips64_exec_SC.

--- unstable/mips64_exec.c
+++ unstable/mips64_exec.c
@@ -510,17 +510,6 @@ forced_inline void mips64_exec_bdslot(cpu_mips_t *cpu)
    cpu->bd_slot = 0;
 }
 
-/* SC (Store Conditional) */
-static fastcall int mips64_exec_SC(cpu_mips_t *cpu,mips_insn_t insn)
-{
-   int base   = bits(insn,21,25);
-   int rt     = bits(insn,16,20);
-   int offset = bits(insn,0,15);
-
-   mips64_exec_memop2(cpu,MIPS_MEMOP_SC,base,offset,rt,TRUE);
-   return(0);
-}
-
 /* SD (Store Double-Word) */
 static fastcall int mips64_exec_SD(cpu_mips_t *cpu,mips_insn_t insn)
 {

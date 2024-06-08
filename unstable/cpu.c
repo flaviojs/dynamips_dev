f7163c27014a7b2d363562c45c5a9ec87ed2a214
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-08 18:49:57 +0100

    Rustify cpu_group_find_highest_id.

--- unstable/cpu.c
+++ unstable/cpu.c
@@ -30,23 +30,6 @@
 #include "ppc32_jit.h"
 #include "dynamips.h"
 
-/* Find the highest CPU ID in a CPU group */
-int cpu_group_find_highest_id(cpu_group_t *group,u_int *highest_id)
-{
-   cpu_gen_t *cpu;
-   u_int max_id = 0;
-
-   if (!group || group->cpu_list)
-      return(-1);
-
-   for(cpu=group->cpu_list;cpu;cpu=cpu->next)
-      if (cpu->id >= max_id)
-         max_id = cpu->id;
-
-   *highest_id = max_id;
-   return(0);
-}
-
 /* Add a CPU in a CPU group */
 int cpu_group_add(cpu_group_t *group,cpu_gen_t *cpu)
 {

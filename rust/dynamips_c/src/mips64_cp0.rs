f8abe05642416cc834dfb784de330c06ec19317c
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-09 08:10:47 +0100

    Rustify mips64_cp0_check_kernel_mode, mips64_cp0_get_mode, mips64_cp0_get_mode_inline, MIPS_CP0_CAUSE_*, MIPS_CP0_STATUS_*, MIPS_CP0_S1_*, MIPS_CP0_*.

--- rust/dynamips_c/src/mips64_cp0.rs
+++ rust/dynamips_c/src/mips64_cp0.rs
@@ -5,7 +5,9 @@
 //! We don't use the JIT here, since there is no high performance needed.
 
 use crate::_private::*;
+use crate::dynamips_common::*;
 use crate::mips64::*;
+use crate::utils::*;
 
 /// MIPS cp0 registers names
 #[no_mangle]
@@ -56,4 +58,110 @@ pub unsafe extern "C" fn mips64_cp0_get_reg_index(name: *mut c_char) -> c_int {
     }
 
     -1
+}
+
+/// Get the CPU operating mode (User,Supervisor or Kernel) - inline version
+#[cfg(not(feature = "USE_UNSTABLE"))]
+#[inline(always)]
+unsafe fn mips64_cp0_get_mode_inline(cpu: *mut cpu_mips_t) -> u_int {
+    let cp0: *mut mips_cp0_t = addr_of_mut!((*cpu).cp0);
+
+    let mut cpu_mode: u_int = ((*cp0).reg[MIPS_CP0_STATUS] >> MIPS_CP0_STATUS_KSU_SHIFT) as u_int;
+    cpu_mode &= MIPS_CP0_STATUS_KSU_MASK;
+    cpu_mode
+}
+
+/// Get the CPU operating mode (User,Supervisor or Kernel)
+#[cfg(not(feature = "USE_UNSTABLE"))]
+unsafe fn mips64_cp0_get_mode(cpu: *mut cpu_mips_t) -> u_int {
+    mips64_cp0_get_mode_inline(cpu)
+}
+
+/// Check that we are running in kernel mode
+#[cfg(not(feature = "USE_UNSTABLE"))]
+pub unsafe fn mips64_cp0_check_kernel_mode(cpu: *mut cpu_mips_t) -> c_int {
+    let cpu_mode: u_int = mips64_cp0_get_mode(cpu);
+
+    if cpu_mode != MIPS_CP0_STATUS_KM {
+        // XXX Branch delay slot
+        mips64_trigger_exception(cpu, MIPS_CP0_CAUSE_ILLOP, 0);
+        return 1;
+    }
+
+    0
+}
+
+/// Get the CPU operating mode (User,Supervisor or Kernel)
+#[cfg(feature = "USE_UNSTABLE")]
+#[inline(always)]
+unsafe fn mips64_cp0_get_mode(cpu: *mut cpu_mips_t) -> u_int {
+    let cp0: *mut mips_cp0_t = addr_of_mut!((*cpu).cp0);
+
+    let mut cpu_mode: u_int = ((*cp0).reg[MIPS_CP0_STATUS] >> MIPS_CP0_STATUS_KSU_SHIFT) as u_int;
+    cpu_mode &= MIPS_CP0_STATUS_KSU_MASK;
+    cpu_mode
+}
+
+/// Check that we are running in kernel mode
+#[cfg(feature = "USE_UNSTABLE")]
+pub unsafe fn mips64_cp0_check_kernel_mode(cpu: *mut cpu_mips_t) -> c_int {
+    let cpu_mode: u_int = mips64_cp0_get_mode(cpu);
+
+    if cpu_mode != MIPS_CP0_STATUS_KM {
+        mips64_general_exception(cpu, MIPS_CP0_CAUSE_ILLOP);
+        return 1;
+    }
+
+    0
+}
+
+/// Get value of random register
+#[no_mangle] // TODO private
+#[inline]
+pub unsafe extern "C" fn mips64_cp0_get_random_reg(cpu: *mut cpu_mips_t) -> u_int {
+    // We use the virtual count register as a basic "random" value
+    let wired: u_int = (*cpu).cp0.reg[MIPS_CP0_WIRED] as u_int;
+    wired + ((*cpu).cp0_virt_cnt_reg % ((*cpu).cp0.tlb_entries - wired))
+}
+
+/// Get a cp0 register (fast version)
+#[no_mangle] // TODO private
+#[inline]
+pub unsafe extern "C" fn mips64_cp0_get_reg_fast(cpu: *mut cpu_mips_t, cp0_reg: u_int) -> m_uint64_t {
+    let cp0: *mut mips_cp0_t = addr_of_mut!((*cpu).cp0);
+    let delta: m_uint32_t;
+    let mut res: m_uint32_t;
+
+    match cp0_reg as usize {
+        MIPS_CP0_COUNT => {
+            delta = (*cpu).cp0_virt_cmp_reg - (*cpu).cp0_virt_cnt_reg;
+            res = (*cp0).reg[MIPS_CP0_COMPARE] as m_uint32_t;
+            res -= (*(*cpu).vm).clock_divisor * delta;
+            sign_extend(res as m_int64_t, 32) as m_uint64_t
+        }
+
+        MIPS_CP0_COMPARE => {
+            if true {
+                sign_extend((*cp0).reg[MIPS_CP0_COMPARE] as m_int64_t, 32) as m_uint64_t
+            } else {
+                // really useful and logical ?
+                delta = (*cpu).cp0_virt_cmp_reg - (*cpu).cp0_virt_cnt_reg;
+                res = (*cp0).reg[MIPS_CP0_COUNT] as m_uint32_t;
+                res += (*(*cpu).vm).clock_divisor * delta;
+                res as m_uint64_t
+            }
+        }
+
+        MIPS_CP0_INFO => MIPS64_R7000_TLB64_ENABLE as m_uint64_t,
+
+        MIPS_CP0_RANDOM => mips64_cp0_get_random_reg(cpu) as m_uint64_t,
+
+        _ => (*cp0).reg[cp0_reg as usize],
+    }
+}
+
+/// Get a cp0 register
+#[no_mangle]
+pub unsafe extern "C" fn mips64_cp0_get_reg(cpu: *mut cpu_mips_t, cp0_reg: u_int) -> m_uint64_t {
+    mips64_cp0_get_reg_fast(cpu, cp0_reg)
 }

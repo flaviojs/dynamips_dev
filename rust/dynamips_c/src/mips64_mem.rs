e5739fe6570b4fd97872f3aa1462e2832588e3b8
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-12 21:38:38 +0100

    Rustify mips64_mem_shutdown.

--- rust/dynamips_c/src/mips64_mem.rs
+++ rust/dynamips_c/src/mips64_mem.rs
@@ -1,2 +1,12 @@
 //! Cisco router simulation platform.
 //! Copyright (c) 2005,2006 Christophe Fillot (cf@utc.fr)
+
+use crate::mips64::*;
+
+/// Shutdown MTS subsystem
+#[no_mangle]
+pub unsafe extern "C" fn mips64_mem_shutdown(cpu: *mut cpu_mips_t) {
+    if (*cpu).mts_shutdown.is_some() {
+        (*cpu).mts_shutdown.unwrap()(cpu);
+    }
+}

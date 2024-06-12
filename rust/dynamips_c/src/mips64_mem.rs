740f5f1e246b66225fb3c97120a418673a879ff2
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-12 22:37:14 +0100

    Rustify mips64_set_addr_mode.

--- rust/dynamips_c/src/mips64_mem.rs
+++ rust/dynamips_c/src/mips64_mem.rs
@@ -1,6 +1,14 @@
 //! MIPS64 MTS subsystem.
 
 use crate::mips64::*;
+use crate::prelude::*;
+
+extern "C" {
+    pub fn mips64_mts32_init(cpu: *mut cpu_mips_t) -> c_int;
+    pub fn mips64_mts32_init_memop_vectors(cpu: *mut cpu_mips_t);
+    pub fn mips64_mts64_init(cpu: *mut cpu_mips_t) -> c_int;
+    pub fn mips64_mts64_init_memop_vectors(cpu: *mut cpu_mips_t);
+}
 
 /// Shutdown MTS subsystem
 #[no_mangle]
@@ -8,4 +16,29 @@ pub unsafe extern "C" fn mips64_mem_shutdown(cpu: *mut cpu_mips_t) {
     if (*cpu).mts_shutdown.is_some() {
         (*cpu).mts_shutdown.unwrap()(cpu);
     }
+}
+
+/// Set the address mode
+#[no_mangle]
+pub unsafe extern "C" fn mips64_set_addr_mode(cpu: *mut cpu_mips_t, addr_mode: u_int) -> c_int {
+    if (*cpu).addr_mode != addr_mode {
+        mips64_mem_shutdown(cpu);
+
+        match addr_mode {
+            32 => {
+                mips64_mts32_init(cpu);
+                mips64_mts32_init_memop_vectors(cpu);
+            }
+            64 => {
+                mips64_mts64_init(cpu);
+                mips64_mts64_init_memop_vectors(cpu);
+            }
+            _ => {
+                libc::fprintf(c_stderr(), cstr!("mts_set_addr_mode: internal error (addr_mode=%u)\n"), addr_mode);
+                libc::exit(libc::EXIT_FAILURE);
+            }
+        }
+    }
+
+    0
 }

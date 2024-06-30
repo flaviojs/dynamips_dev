1939ccc7d9b7e647b0f563cfa553a47a4ff3e3b5
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-06-30 15:53:08 +0100

    Rustify atm_aal5_send, atm_aal5_add_trailer, atm_store_fifo, atm_send_fifo, atm_add_tx_padding, atm_clear_tx_fifo, atm_send_cell, atm_seg_context.

--- rust/dynamips_c/src/atm_vsar.rs
+++ rust/dynamips_c/src/atm_vsar.rs
@@ -1,7 +1,12 @@
 //! ATM Virtual Segmentation & Reassembly Engine.
 
+use crate::atm::*;
+use crate::crc::*;
 use crate::dynamips_common::*;
+use crate::net_io::*;
 use crate::prelude::*;
+use crate::utils::*;
+use std::cmp::min;
 
 pub const ATM_REAS_MAX_SIZE: usize = 16384;
 
@@ -14,9 +19,132 @@ pub struct atm_reas_context {
     pub len: size_t,
 }
 
+/// Segmentation Context
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct atm_seg_context {
+    pub nio: *mut netio_desc_t,
+    pub txfifo_cell: [m_uint8_t; ATM_CELL_SIZE],
+    pub txfifo_pos: size_t,
+    pub txfifo_avail: size_t,
+    pub aal5_len: size_t,
+    pub aal5_crc: m_uint32_t,
+    pub atm_hdr: m_uint32_t,
+    pub buffer: *mut c_char,
+    pub buf_len: size_t,
+}
+
 /// Reset a receive context
 #[no_mangle]
 pub unsafe extern "C" fn atm_aal5_recv_reset(arc: *mut atm_reas_context) {
     (*arc).buf_pos = 0;
     (*arc).len = 0;
+}
+
+/// Send the ATM cell in FIFO
+unsafe fn atm_send_cell(asc: *mut atm_seg_context) {
+    m_hton32((*asc).txfifo_cell.as_c_mut(), (*asc).atm_hdr);
+    atm_insert_hec((*asc).txfifo_cell.as_c_mut());
+    netio_send((*asc).nio, (*asc).txfifo_cell.as_c_void_mut(), ATM_CELL_SIZE);
+}
+
+/// Clear the TX fifo
+unsafe fn atm_clear_tx_fifo(asc: *mut atm_seg_context) {
+    (*asc).txfifo_avail = ATM_PAYLOAD_SIZE;
+    (*asc).txfifo_pos = ATM_HDR_SIZE;
+    libc::memset((*asc).txfifo_cell.as_c_void_mut(), 0, ATM_CELL_SIZE);
+}
+
+/// Add padding to the FIFO
+unsafe fn atm_add_tx_padding(asc: *mut atm_seg_context, mut len: size_t) {
+    if len > (*asc).txfifo_avail {
+        len = (*asc).txfifo_avail;
+    }
+
+    libc::memset(addr_of_mut!((*asc).txfifo_cell[(*asc).txfifo_pos]).cast::<_>(), 0, len);
+    (*asc).txfifo_pos += len;
+    (*asc).txfifo_avail -= len;
+}
+
+/// Send the TX fifo if it is empty
+unsafe fn atm_send_fifo(asc: *mut atm_seg_context) {
+    if (*asc).txfifo_avail == 0 {
+        (*asc).aal5_crc = crc32_compute(!(*asc).aal5_crc, addr_of_mut!((*asc).txfifo_cell[ATM_HDR_SIZE]), ATM_PAYLOAD_SIZE as c_int);
+        atm_send_cell(asc);
+        atm_clear_tx_fifo(asc);
+    }
+}
+
+/// Store a packet in the TX FIFO
+unsafe fn atm_store_fifo(asc: *mut atm_seg_context) -> c_int {
+    let len: size_t = min((*asc).buf_len, (*asc).txfifo_avail);
+
+    libc::memcpy(addr_of_mut!((*asc).txfifo_cell[(*asc).txfifo_pos]).cast::<_>(), (*asc).buffer.cast::<_>(), len);
+    (*asc).buffer = (*asc).buffer.add(len);
+    (*asc).buf_len -= len;
+    (*asc).txfifo_pos += len;
+    (*asc).txfifo_avail -= len;
+
+    if (*asc).txfifo_avail == 0 {
+        atm_send_fifo(asc);
+        return TRUE;
+    }
+
+    FALSE
+}
+
+/// Add the AAL5 trailer to the TX FIFO
+unsafe fn atm_aal5_add_trailer(asc: *mut atm_seg_context) {
+    let trailer: *mut m_uint8_t = addr_of_mut!((*asc).txfifo_cell[ATM_AAL5_TRAILER_POS]);
+
+    // Control field + Length
+    m_hton32(trailer, (*asc).aal5_len as m_uint32_t);
+
+    // Final CRC-32 computation
+    (*asc).aal5_crc = crc32_compute(!(*asc).aal5_crc, addr_of_mut!((*asc).txfifo_cell[ATM_HDR_SIZE]), (ATM_PAYLOAD_SIZE - 4) as c_int);
+
+    m_hton32(trailer.add(4), (*asc).aal5_crc);
+
+    // Consider the FIFO as full
+    (*asc).txfifo_avail = 0;
+}
+
+/// Send an AAL5 packet through an NIO (segmentation)
+#[no_mangle]
+pub unsafe extern "C" fn atm_aal5_send(nio: *mut netio_desc_t, vpi: u_int, vci: u_int, iov: *mut libc::iovec, iovcnt: c_int) -> c_int {
+    let mut asc: atm_seg_context = zeroed::<_>();
+
+    asc.nio = nio;
+    asc.aal5_len = 0;
+    asc.aal5_crc = 0; // will be inverted by first CRC update
+    atm_clear_tx_fifo(addr_of_mut!(asc));
+
+    // prepare the atm header
+    asc.atm_hdr = vpi << ATM_HDR_VPI_SHIFT;
+    asc.atm_hdr |= vci << ATM_HDR_VCI_SHIFT;
+
+    for i in 0..iovcnt {
+        asc.buffer = (*iov.offset(i as isize)).iov_base.cast::<_>();
+        asc.buf_len = (*iov.offset(i as isize)).iov_len;
+        asc.aal5_len += (*iov.offset(i as isize)).iov_len;
+
+        while asc.buf_len > 0 {
+            atm_store_fifo(addr_of_mut!(asc));
+        }
+    }
+
+    // Add the PDU trailer. If we have enough room, add it in the last cell,
+    // otherwise create a new one.
+    if asc.txfifo_avail < ATM_AAL5_TRAILER_SIZE {
+        atm_add_tx_padding(addr_of_mut!(asc), asc.txfifo_avail);
+        atm_send_fifo(addr_of_mut!(asc));
+    }
+
+    // Set AAL5 end of packet in ATM header (PTI field)
+    asc.atm_hdr |= ATM_PTI_EOP;
+
+    atm_add_tx_padding(addr_of_mut!(asc), asc.txfifo_avail - ATM_AAL5_TRAILER_SIZE);
+    atm_aal5_add_trailer(addr_of_mut!(asc));
+    atm_send_cell(addr_of_mut!(asc));
+    0
 }

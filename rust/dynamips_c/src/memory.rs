97d9199709d6899be67804aeed69e2c64e220a56
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-07-22 07:05:08 +0100

    Rustify MTS_ACC_*.

--- rust/dynamips_c/src/memory.rs
+++ rust/dynamips_c/src/memory.rs
@@ -12,3 +12,8 @@ extern "C" {
 /// MTS operation
 pub const MTS_READ: u_int = 0;
 pub const MTS_WRITE: u_int = 1;
+
+/// Memory access flags
+pub const MTS_ACC_AE: u_int = 0x00000002; // Address Error
+pub const MTS_ACC_T: u_int = 0x00000004; // TLB Exception
+pub const MTS_ACC_U: u_int = 0x00000006; // Unexistent

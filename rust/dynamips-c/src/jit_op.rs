cfc4379dae6dd64049dfb1c6138f0a4bc9621369
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-08-16 21:10:45 +0100

    Rustify jit_op.c.

--- rust/dynamips-c/src/jit_op.rs
+++ rust/dynamips-c/src/jit_op.rs
@@ -4,6 +4,8 @@
 //! JIT operations.
 
 use crate::_private::*;
+use crate::cpu::*;
+use crate::dynamips_common::*;
 
 pub type jit_op_t = jit_op;
 
@@ -64,4 +66,89 @@ pub unsafe extern "C" fn jit_op_find_opcode(op_list: *mut jit_op_t, opcode: u_in
     }
 
     null_mut()
+}
+
+#[no_mangle]
+pub static mut jit_op_blk_sizes: [u_int; JIT_OP_POOL_NR] = [0, 32, 64, 128, 256, 384, 512, 1024];
+
+/// Get a JIT op (allocate one if necessary)
+#[no_mangle]
+pub unsafe extern "C" fn jit_op_get(cpu: *mut cpu_gen_t, size_index: c_int, opcode: u_int) -> *mut jit_op_t {
+    assert!((size_index as usize) < JIT_OP_POOL_NR);
+    let mut op: *mut jit_op_t = (*cpu).jit_op_pool[size_index as usize];
+
+    if !op.is_null() {
+        assert!((*op).ob_size_index == size_index as m_uint32_t);
+        (*cpu).jit_op_pool[size_index as usize] = (*op).next;
+    } else {
+        // no block found, allocate one
+        let len: size_t = size_of::<jit_op_t>() + jit_op_blk_sizes[size_index as usize] as size_t;
+
+        op = libc::malloc(len).cast::<_>();
+        assert!(!op.is_null());
+        (*op).ob_size_index = size_index as m_uint32_t;
+    }
+
+    (*op).opcode = opcode;
+    (*op).param[0] = -1;
+    (*op).param[1] = -1;
+    (*op).param[2] = -1;
+    (*op).next = null_mut();
+    (*op).ob_ptr = (*op).ob_data.as_c_mut();
+    (*op).arg_ptr = null_mut();
+    (*op).insn_name = null_mut();
+    op
+}
+
+/// Release a JIT op
+#[no_mangle]
+pub unsafe extern "C" fn jit_op_free(cpu: *mut cpu_gen_t, op: *mut jit_op_t) {
+    assert!(((*op).ob_size_index as usize) < JIT_OP_POOL_NR);
+    (*op).next = (*cpu).jit_op_pool[(*op).ob_size_index as usize];
+    (*cpu).jit_op_pool[(*op).ob_size_index as usize] = op;
+}
+
+/// Free a list of JIT ops
+#[no_mangle]
+pub unsafe extern "C" fn jit_op_free_list(cpu: *mut cpu_gen_t, op_list: *mut jit_op_t) {
+    let mut op: *mut jit_op_t;
+    let mut opn: *mut jit_op_t;
+
+    op = op_list;
+    while !op.is_null() {
+        opn = (*op).next;
+        jit_op_free(cpu, op);
+        op = opn;
+    }
+}
+
+/// Initialize JIT op pools for the specified CPU
+#[no_mangle]
+pub unsafe extern "C" fn jit_op_init_cpu(cpu: *mut cpu_gen_t) -> c_int {
+    (*cpu).jit_op_array = libc::calloc((*cpu).jit_op_array_size as size_t, size_of::<*mut jit_op_t>()).cast::<_>();
+
+    if !(*cpu).jit_op_array.is_null() {
+        return -1;
+    }
+
+    libc::memset((*cpu).jit_op_pool.as_c_void_mut(), 0, size_of::<[*mut jit_op_t; JIT_OP_POOL_NR]>());
+    0
+}
+
+/// Free memory used by pools
+#[no_mangle]
+pub unsafe extern "C" fn jit_op_free_pools(cpu: *mut cpu_gen_t) {
+    let mut op: *mut jit_op_t;
+    let mut opn: *mut jit_op_t;
+
+    for i in 0..JIT_OP_POOL_NR {
+        op = (*cpu).jit_op_pool[i];
+        while !op.is_null() {
+            opn = (*op).next;
+            libc::free(op.cast::<_>());
+            op = opn;
+        }
+
+        (*cpu).jit_op_pool[i] = null_mut();
+    }
 }

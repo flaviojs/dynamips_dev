511eb19cb24c9907935563c9a20f4f6b947095c5
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-08-22 17:33:39 +0100

    Rustify dev_ti2050b.

--- common/dev_ti2050b.c
+++ common/dev_ti2050b.c
@@ -16,20 +16,3 @@
 #include "dynamips.h"
 #include "memory.h"
 #include "device.h"
-
-#define PCI_VENDOR_TI         0x104C
-#define PCI_PRODUCT_PCI2050B  0xAC28
-
-/*
- * dev_ti2050b_init()
- */
-int dev_ti2050b_init(struct pci_bus *pci_bus,int pci_device,
-                     struct pci_bus *sec_bus)
-{
-   struct pci_device *dev;
-   
-   dev = pci_bridge_create_dev(pci_bus,"ti2050b",
-                               PCI_VENDOR_TI,PCI_PRODUCT_PCI2050B,
-                               pci_device,0,sec_bus,NULL,NULL);
-   return((dev != NULL) ? 0 : -1);
-}
--- rust/dynamips-c/src/dev_ti2050b.rs
+++ rust/dynamips-c/src/dev_ti2050b.rs
@@ -1,1 +1,21 @@
+//! Cisco router simulation platform.
+//! Copyright (c) 2005,2006 Christophe Fillot (cf@utc.fr)
+//!
+//! Texas Instruments PCI205B PCI bridge.
 
+use crate::_private::*;
+use crate::pci_dev::*;
+
+const PCI_VENDOR_TI: u_int = 0x104C;
+const PCI_PRODUCT_PCI2050B: u_int = 0xAC28;
+
+/// dev_ti2050b_init()
+#[no_mangle]
+pub unsafe extern "C" fn dev_ti2050b_init(pci_bus: *mut pci_bus, pci_device: c_int, sec_bus: *mut pci_bus) -> c_int {
+    let dev: *mut pci_device = pci_bridge_create_dev(pci_bus, cstr!("ti2050b"), PCI_VENDOR_TI, PCI_PRODUCT_PCI2050B, pci_device, 0, sec_bus, None, None);
+    if !dev.is_null() {
+        0
+    } else {
+        -1
+    }
+}

463df2ccbb744525dc51808fb4a020b03863b73d
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-08-23 01:15:11 +0100

    Rustify dev_plx6520cb.

--- common/dev_plx6520cb.c
+++ common/dev_plx6520cb.c
@@ -17,21 +17,3 @@
 #include "dynamips.h"
 #include "memory.h"
 #include "device.h"
-
-#define PCI_VENDOR_PLX          0x10b5
-#define PCI_PRODUCT_PLX_6520CB  0x6520
-
-
-/*
- * dev_plx6520cb_init()
- */
-int dev_plx6520cb_init(struct pci_bus *pci_bus,int pci_device,
-                       struct pci_bus *sec_bus)
-{
-   struct pci_device *dev;
-   
-   dev = pci_bridge_create_dev(pci_bus,"plx6520cb",
-                               PCI_VENDOR_PLX,PCI_PRODUCT_PLX_6520CB,
-                               pci_device,0,sec_bus,NULL,NULL);
-   return((dev != NULL) ? 0 : -1);
-}
--- rust/dynamips-c/src/dev_plx6520cb.rs
+++ rust/dynamips-c/src/dev_plx6520cb.rs
@@ -1,1 +1,22 @@
+//! Cisco router simulation platform.
+//! Copyright (c) 2005,2006 Christophe Fillot (cf@utc.fr)
+//!
+//! PLX6520CB PCI bridge.
+//! This is just a fake device.
 
+use crate::_private::*;
+use crate::pci_dev::*;
+
+const PCI_VENDOR_PLX: u_int = 0x10b5;
+const PCI_PRODUCT_PLX_6520CB: u_int = 0x6520;
+
+/// dev_plx6520cb_init()
+#[no_mangle]
+pub unsafe extern "C" fn dev_plx6520cb_init(pci_bus: *mut pci_bus, pci_device: c_int, sec_bus: *mut pci_bus) -> c_int {
+    let dev: *mut pci_device = pci_bridge_create_dev(pci_bus, cstr!("plx6520cb"), PCI_VENDOR_PLX, PCI_PRODUCT_PLX_6520CB, pci_device, 0, sec_bus, None, None);
+    if !dev.is_null() {
+        0
+    } else {
+        -1
+    }
+}

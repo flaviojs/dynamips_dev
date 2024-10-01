a5321e3f6d940e32661572b7ff619b4a4f881c99
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-10-01 12:58:06 +0100

    Remove force_align_arg_pointer function attribute.

--- stable/mips_mts.c
+++ stable/mips_mts.c
@@ -144,7 +144,6 @@ static void *MTS_PROTO(lookup)(cpu_mips_t *cpu,m_uint64_t vaddr)
 /* === MIPS Memory Operations ============================================= */
 
 /* LB: Load Byte */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lb)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -156,7 +155,6 @@ void MTS_PROTO(lb)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LBU: Load Byte Unsigned */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lbu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -168,7 +166,6 @@ void MTS_PROTO(lbu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LH: Load Half-Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lh)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -180,7 +177,6 @@ void MTS_PROTO(lh)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LHU: Load Half-Word Unsigned */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lhu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -192,7 +188,6 @@ void MTS_PROTO(lhu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LW: Load Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lw)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -204,7 +199,6 @@ void MTS_PROTO(lw)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LWU: Load Word Unsigned */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lwu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -216,7 +210,6 @@ void MTS_PROTO(lwu)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LD: Load Double-Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(ld)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -228,7 +221,6 @@ void MTS_PROTO(ld)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SB: Store Byte */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sb)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -240,7 +232,6 @@ void MTS_PROTO(sb)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SH: Store Half-Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sh)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -252,7 +243,6 @@ void MTS_PROTO(sh)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SW: Store Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sw)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -264,7 +254,6 @@ void MTS_PROTO(sw)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SD: Store Double-Word */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sd)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -276,7 +265,6 @@ void MTS_PROTO(sd)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LDC1: Load Double-Word To Coprocessor 1 */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(ldc1)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -288,7 +276,6 @@ void MTS_PROTO(ldc1)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LWL: Load Word Left */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lwl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t r_mask,naddr;
@@ -312,7 +299,6 @@ void MTS_PROTO(lwl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LWR: Load Word Right */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(lwr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t r_mask,naddr;
@@ -337,7 +323,6 @@ void MTS_PROTO(lwr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LDL: Load Double-Word Left */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(ldl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t r_mask,naddr;
@@ -360,7 +345,6 @@ void MTS_PROTO(ldl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LDR: Load Double-Word Right */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(ldr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t r_mask,naddr;
@@ -383,7 +367,6 @@ void MTS_PROTO(ldr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SWL: Store Word Left */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(swl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t d_mask,naddr;
@@ -408,7 +391,6 @@ void MTS_PROTO(swl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SWR: Store Word Right */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(swr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t d_mask,naddr;
@@ -433,7 +415,6 @@ void MTS_PROTO(swr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SDL: Store Double-Word Left */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sdl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t d_mask,naddr;
@@ -458,7 +439,6 @@ void MTS_PROTO(sdl)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SDR: Store Double-Word Right */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sdr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t d_mask,naddr;
@@ -483,7 +463,6 @@ void MTS_PROTO(sdr)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* LL: Load Linked */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(ll)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -497,7 +476,6 @@ void MTS_PROTO(ll)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SC: Store Conditional */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sc)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -513,7 +491,6 @@ void MTS_PROTO(sc)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* SDC1: Store Double-Word from Coprocessor 1 */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(sdc1)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -525,7 +502,6 @@ void MTS_PROTO(sdc1)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int reg)
 }
 
 /* CACHE: Cache operation */
-__attribute__((force_align_arg_pointer))
 void MTS_PROTO(cache)(cpu_mips_t *cpu,m_uint64_t vaddr,u_int op)
 {
    mips64_jit_tcb_t *block;
--- stable/ppc32_mem.c
+++ stable/ppc32_mem.c
@@ -726,7 +726,6 @@ static void ppc405_dump_tlb(cpu_gen_t *cpu)
 /* === PPC Memory Operations ============================================= */
 
 /* LBZ: Load Byte Zero */
-__attribute__((force_align_arg_pointer))
 void ppc32_lbz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -738,7 +737,6 @@ void ppc32_lbz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LHZ: Load Half-Word Zero */
-__attribute__((force_align_arg_pointer))
 void ppc32_lhz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -750,7 +748,6 @@ void ppc32_lhz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LWZ: Load Word Zero */
-__attribute__((force_align_arg_pointer))
 void ppc32_lwz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -762,7 +759,6 @@ void ppc32_lwz(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LWBR: Load Word Byte Reverse */
-__attribute__((force_align_arg_pointer))
 void ppc32_lwbr(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -774,7 +770,6 @@ void ppc32_lwbr(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LHA: Load Half-Word Algebraic */
-__attribute__((force_align_arg_pointer))
 void ppc32_lha(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -786,7 +781,6 @@ void ppc32_lha(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STB: Store Byte */
-__attribute__((force_align_arg_pointer))
 void ppc32_stb(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -798,7 +792,6 @@ void ppc32_stb(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STH: Store Half-Word */
-__attribute__((force_align_arg_pointer))
 void ppc32_sth(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -810,7 +803,6 @@ void ppc32_sth(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STW: Store Word */
-__attribute__((force_align_arg_pointer))
 void ppc32_stw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -822,7 +814,6 @@ void ppc32_stw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STWBR: Store Word Byte Reversed */
-__attribute__((force_align_arg_pointer))
 void ppc32_stwbr(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -834,7 +825,6 @@ void ppc32_stwbr(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LSW: Load String Word */
-__attribute__((force_align_arg_pointer))
 void ppc32_lsw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -846,7 +836,6 @@ void ppc32_lsw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STW: Store String Word */
-__attribute__((force_align_arg_pointer))
 void ppc32_stsw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -858,7 +847,6 @@ void ppc32_stsw(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* LFD: Load Floating-Point Double */
-__attribute__((force_align_arg_pointer))
 void ppc32_lfd(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -870,7 +858,6 @@ void ppc32_lfd(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* STFD: Store Floating-Point Double */
-__attribute__((force_align_arg_pointer))
 void ppc32_stfd(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 {
    m_uint64_t data;
@@ -882,7 +869,6 @@ void ppc32_stfd(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int reg)
 }
 
 /* ICBI: Instruction Cache Block Invalidate */
-__attribute__((force_align_arg_pointer))
 void ppc32_icbi(cpu_ppc_t *cpu,m_uint32_t vaddr,u_int op)
 {
    ppc32_jit_tcb_t *block;

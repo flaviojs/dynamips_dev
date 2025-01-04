e5683cca5173f82f1e8457a783196631dd450855
Flávio J. Saraiva <flaviojs2005@gmail.com>
2025-01-04 08:24:10 +0000

    Rustify ptask.

--- common/ptask.h
+++ common/ptask.h
@@ -10,34 +10,4 @@
 
 #include "dynamips_c.h"
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-
-/* ptask identifier */
-typedef m_int64_t ptask_id_t;
-
-/* periodic task callback prototype */
-typedef int (*ptask_callback)(void *object,void *arg);
-
-/* periodic task definition */
-typedef struct ptask ptask_t;
-struct ptask {
-   ptask_id_t id;
-   ptask_t *next;
-   ptask_callback cbk;
-   void *object,*arg;
-};
-
-extern u_int ptask_sleep_time;
-
-/* Add a new task */
-ptask_id_t ptask_add(ptask_callback cbk,void *object,void *arg);
-
-/* Remove a task */
-int ptask_remove(ptask_id_t id);
-
-/* Initialize ptask module */
-int ptask_init(u_int sleep_time);
-
 #endif

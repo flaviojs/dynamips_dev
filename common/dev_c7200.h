60b19a6f7ee5be83f07712536ff58647723f1e72
Flávio J. Saraiva <flaviojs2005@gmail.com>
2025-01-06 21:13:59 +0000

    Remove dev_ds1620.

--- common/dev_c7200.h
+++ common/dev_c7200.h
@@ -29,7 +29,6 @@
 #include "device.h"
 #include "pci_dev.h"
 #include "dev_mv64460.h"
-#include "dev_ds1620.h"
 #include "net_io.h"
 #include "vm.h"
 
--- common/dev_ds1620.c
+++
@@ -1,8 +0,0 @@
-/*
- * Cisco router simulation platform.
- * Copyright (c) 2005,2006 Christophe Fillot (cf@utc.fr)
- *
- * Dallas DS1620 Temperature sensors.
- */
-
-#include "dynamips_c.h"
